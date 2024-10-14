deccde70664021696280a5c75873a79fb1153067
ArthurColleu <colleuarthur@gmail.com>
2024-10-14 15:51:53 +0200

    Q15 terminé

--- src/Vue/Vue_Menu_Administration.php
+++ src/Vue/Vue_Menu_Administration.php
@@ -4,23 +4,33 @@ use App\Utilitaire\Vue_Composant;
 
 class Vue_Menu_Administration extends Vue_Composant
 {
-    public function __construct( )
-    {           }
+    private string $typeDeVue = "";
+
+    public function __construct(string $typeDeVue)
+    {
+        $this->typeDeVue = $typeDeVue;
+    }
+
     function donneTexte(): string
     {
-
+        switch ($this->typeDeVue) {
+            case "administrateurLogiciel":
                 return "
              <nav id='menu'>
               <ul id='menu-closed'> 
                 <li><a href='?case=Gerer_utilisateur'>Utilisateurs</a></li>
-                <li><a href='?case=Gerer_catalogue'>Catalogue</a></li>   
-             <li><a href='?case=Gerer_entreprisesPartenaires'>Entreprises partenaires</a></li>
-               <li><a href='?case=Gerer_Commande'>Commandes</a></li>
-            
                 <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
                </ul>
-            </nav> 
+            </nav> ";
+            case "gestionnaireCatalogue":
+                return "
+             <nav id='menu'>
+              <ul id='menu-closed'> 
+                <li><a href='?case=Gerer_catalogue'>Catalogue</a></li>   
+                <li><a href='?case=Gerer_monCompte'>Mon compte</a></li> 
+               </ul>
+            </nav>
 ";
-              
+        }
     }
 }
